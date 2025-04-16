3924c16d6c067dff162c264b38930d864fc60187
nuriavt1 <134273029+nuriavt1@users.noreply.github.com>
2025-04-16 13:19:59 +0200

    afegir llibre
    He creat els botons per anar a les altres pagines d'afegir llibre

--- BookEchoApp/screens/AddScreen.js
+++ BookEchoApp/screens/AddScreen.js
@@ -1,24 +1,77 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+// Importamos los íconos de Expo
+import { Ionicons, AntDesign } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const AddScreen = () => {
+    const navigation = useNavigation();
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Add Screen</Text>
+            <Text style={styles.title}>Afegir llibre</Text>
+           
+            <TouchableOpacity style={styles.option} onPress={() => navigation.navigate("Stack")}>
+           <View>
+                <View style={styles.row}>
+                    <AntDesign name="search1" size={24} color="black" />
+                    <Text style={styles.buttonText}>Per cerca</Text>
+                </View>
+           </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.option} onPress={() => navigation.navigate("Stack")}>
+           <View>
+                <View style={styles.row}>
+                    <AntDesign name="camerao" size={24} color="black" />
+                    <Text style={styles.buttonText}>Per escaneig</Text> 
+                </View>
+           </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.option} onPress={() => navigation.navigate("Stack")}>
+           <View>
+                <View style={styles.row}>
+                <MaterialCommunityIcons name="keyboard-outline" size={24} color="black" />
+
+                    <Text style={styles.buttonText}>Per entrada</Text>
+                </View>
+           </View>
+            </TouchableOpacity>
+
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1, 
+        flex: 1,
         justifyContent: "center",
         alignItems: "center",
     },
     title: {
         fontSize: 30,
         textAlign: "center",
+        marginBottom: 20,
     },
+
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-start",
+
+    },
+    buttonText: {
+        marginLeft: 10,
+    },
+    option: {
+        width: '95%',           
+        borderTopWidth: 1,         
+        borderTopColor: 'green',   
+        paddingVertical: 15, 
+        paddingHorizontal: 20,
+      },
+
 });
 
 export default AddScreen;
