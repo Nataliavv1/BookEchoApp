991f6a11d067be864376c3651ddcd1105621ccf4
Nataliavv1 <natalia.vera.villo@estudiantat.upc.edu>
2025-04-23 09:49:56 +0200

    variables colors

---
+++ BookEchoApp/styles/colors.js
@@ -0,0 +1,102 @@
+// Definim els colors tenint en compte les variants del Figma
+
+const colors = {
+    // Orange
+    LightOrange: '#fef2ed',
+    LightHoverOrange: '#feebe4',
+    LightActiveOrange: '#fdd5c7',
+    NormalOrange: '#f8794a',
+    NormalHoverOrange: '#df6d43',
+    NormalActiveOrange: '#c6613b',
+    DarkOrange: '#ba5b38',
+    DarkHoverOrange: '#95492c',
+    DarkActiveOrange: '#703621',
+    DarkerOrange: '#572a1a',
+  
+    // Turquoise
+    LightTurquoise: '#edf7f5',
+    LightHoverTurquoise: '#e3f3f0',
+    LightActiveTurquoise: '#c6e5e1',
+    NormalTurquoise: '#47ac9e',
+    NormalHoverTurquoise: '#409b8e',
+    NormalActiveTurquoise: '#398a7e',
+    DarkTurquoise: '#358177',
+    DarkHoverTurquoise: '#2b675f',
+    DarkActiveTurquoise: '#204d47',
+    DarkerTurquoise: '#193c37',
+  
+    // Yellow
+    LightYellow: '#fef8e6',
+    LightHoverYellow: '#fef5d9',
+    LightActiveYellow: '#fdebb0',
+    NormalYellow: '#f8bd01',
+    NormalHoverYellow: '#dfaa01',
+    NormalActiveYellow: '#c69701',
+    DarkYellow: '#ba8e01',
+    DarkHoverYellow: '#957101',
+    DarkActiveYellow: '#705500',
+    DarkerYellow: '#574200',
+  
+    // Grey
+    LightGrey: '#efefef',
+    LightHoverGrey: '#e7e7e7',
+    LightActiveGrey: '#cecece',
+    NormalGrey: '#626262',
+    NormalHoverGrey: '#585858',
+    NormalActiveGrey: '#4e4e4e',
+    DarkGrey: '#4a4a4a',
+    DarkHoverGrey: '#3b3b3b',
+    DarkActiveGrey: '#2c2c2c',
+    DarkerGrey: '#222222',
+  
+    // White
+    LightWhite: '#fefefe',
+    LightHoverWhite: '#fefefe',
+    LightActiveWhite: '#fdfdfd',
+    NormalWhite: '#f7f7f7',
+    NormalHoverWhite: '#dedede',
+    NormalActiveWhite: '#c6c6c6',
+    DarkWhite: '#b9b9b9',
+    DarkHoverWhite: '#949494',
+    DarkActiveWhite: '#6f6f6f',
+    DarkerWhite: '#565656',
+  
+    // Green
+    LightGreen: '#eff7ed',
+    LightHoverGreen: '#e7f3e3',
+    LightActiveGreen: '#cde5c6',
+    NormalGreen: '#5dac47',
+    NormalHoverGreen: '#549b40',
+    NormalActiveGreen: '#4a8a39',
+    DarkGreen: '#468135',
+    DarkHoverGreen: '#38672b',
+    DarkActiveGreen: '#2a4d20',
+    DarkerGreen: '#213c19',
+  
+    // Purple
+    LightPurple: '#f7edf7',
+    LightHoverPurple: '#f3e3f3',
+    LightActivePurple: '#e5c6e5',
+    NormalPurple: '#ac47ac',
+    NormalHoverPurple: '#9b409b',
+    NormalActivePurple: '#8a398a',
+    DarkPurple: '#813581',
+    DarkHoverPurple: '#672b67',
+    DarkActivePurple: '#4d204d',
+    DarkerPurple: '#3c196c',
+  
+    // Red
+    LightRed: '#feedf7',
+    LightHoverRed: '#fee4f3',
+    LightActiveRed: '#fdc7e7',
+    NormalRed: '#f84ab2',
+    NormalHoverRed: '#df43a0',
+    NormalActiveRed: '#c63b8e',
+    DarkRed: '#ba3886',
+    DarkHoverRed: '#952c6b',
+    DarkActiveRed: '#702150',
+    DarkerRed: '#571a3e',
+  };
+  
+  export default colors;
+  
