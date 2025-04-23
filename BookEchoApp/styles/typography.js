1c19ec5c04e13d343a08463c591660f70ee102c5
Nataliavv1 <natalia.vera.villo@estudiantat.upc.edu>
2025-04-23 10:12:09 +0200

    tipografia variables

---
+++ BookEchoApp/styles/typography.js
@@ -0,0 +1,372 @@
+// Definim les fonts i estils
+
+const typography = {
+    // Fonts base Urbanist i Raleway
+    fontFamilyUrbanist: {
+      light: 'Urbanist_300Light',
+      regular: 'Urbanist_400Regular',
+      medium: 'Urbanist_500Medium',
+      semibold: 'Urbanist_600SemiBold',
+      bold: 'Urbanist_700Bold',
+      extrabold: 'Urbanist_800ExtraBold',
+      black: 'Urbanist_900Black',
+    },
+    fontFamilyRaleway: {
+      light: 'Raleway_300Light',
+      regular: 'Raleway_400Regular',
+      medium: 'Raleway_500Medium',
+      semibold: 'Raleway_600SemiBold',
+      bold: 'Raleway_700Bold',
+      extrabold: 'Raleway_800ExtraBold',
+      black: 'Raleway_900Black',
+    },
+  
+    // H1
+    H1Light: {
+      fontSize: 24,
+      lineHeight: 28,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_300Light',
+    },
+    H1Regular: {
+      fontSize: 24,
+      lineHeight: 28,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_400Regular',
+    },
+    H1Medium: {
+      fontSize: 24,
+      lineHeight: 28,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_500Medium',
+    },
+    H1SemiBold: {
+      fontSize: 24,
+      lineHeight: 28,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_600SemiBold',
+    },
+    H1Bold: {
+      fontSize: 24,
+      lineHeight: 28,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_700Bold',
+    },
+    H1ExtraBold: {
+      fontSize: 24,
+      lineHeight: 28,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_800ExtraBold',
+    },
+    H1Black: {
+      fontSize: 24,
+      lineHeight: 28,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_900Black',
+    },
+  
+    // H2
+    H2Light: {
+      fontSize: 20,
+      lineHeight: 24,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_300Light',
+    },
+    H2Regular: {
+      fontSize: 20,
+      lineHeight: 24,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_400Regular',
+    },
+    H2Medium: {
+      fontSize: 20,
+      lineHeight: 24,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_500Medium',
+    },
+    H2SemiBold: {
+      fontSize: 20,
+      lineHeight: 24,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_600SemiBold',
+    },
+    H2Bold: {
+      fontSize: 20,
+      lineHeight: 24,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_700Bold',
+    },
+    H2ExtraBold: {
+      fontSize: 20,
+      lineHeight: 24,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_800ExtraBold',
+    },
+    H2Black: {
+      fontSize: 20,
+      lineHeight: 24,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_900Black',
+    },
+  
+    // H3
+    H3Light: {
+      fontSize: 18,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_300Light',
+    },
+    H3Regular: {
+      fontSize: 18,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_400Regular',
+    },
+    H3Medium: {
+      fontSize: 18,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_500Medium',
+    },
+    H3SemiBold: {
+      fontSize: 18,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_600SemiBold',
+    },
+    H3Bold: {
+      fontSize: 18,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_700Bold',
+    },
+    H3ExtraBold: {
+      fontSize: 18,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_800ExtraBold',
+    },
+    H3Black: {
+      fontSize: 18,
+      lineHeight: 22,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_900Black',
+    },
+  
+    // Subtitle
+    subtitleLight: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_300Light',
+    },
+    subtitleRegular: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_400Regular',
+    },
+    subtitleMedium: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_500Medium',
+    },
+    subtitleSemiBold: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_600SemiBold',
+    },
+    subtitleBold: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_700Bold',
+    },
+    subtitleExtraBold: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_800ExtraBold',
+    },
+    subtitleBlack: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_900Black',
+    },
+  
+    // Body
+    bodyLight: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_300Light',
+    },
+    bodyRegular: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_400Regular',
+    },
+    bodyMedium: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_500Medium',
+    },
+    bodySemiBold: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_600SemiBold',
+    },
+    bodyBold: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_700Bold',
+    },
+    bodyExtraBold: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_800ExtraBold',
+    },
+    bodyBlack: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_900Black',
+    },
+  
+    // Button
+    buttonLight: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_300Light',
+    },
+    buttonRegular: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_400Regular',
+    },
+    buttonMedium: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_500Medium',
+    },
+    buttonSemiBold: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_600SemiBold',
+    },
+    buttonBold: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_700Bold',
+    },
+    buttonExtraBold: {
+      fontSize: 16,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Urbanist_800ExtraBold',
+    },
+  
+    // Label
+    labelLight: {
+      fontSize: 14,
+      lineHeight: 18,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_300Light',
+    },
+    labelRegular: {
+      fontSize: 14,
+      lineHeight: 18,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_400Regular',
+    },
+    labelMedium: {
+      fontSize: 14,
+      lineHeight: 18,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_500Medium',
+    },
+    labelSemiBold: {
+      fontSize: 14,
+      lineHeight: 18,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_600SemiBold',
+    },
+    labelBold: {
+      fontSize: 14,
+      lineHeight: 18,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_700Bold',
+    },
+    labelExtraBold: {
+      fontSize: 14,
+      lineHeight: 18,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_800ExtraBold',
+    },
+    labelBlack: {
+      fontSize: 14,
+      lineHeight: 20,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_900Black',
+    },
+  
+    // Footer
+    footerLight: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_300Light',
+    },
+    footerRegular: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_400Regular',
+    },
+    footerMedium: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_500Medium',
+    },
+    footerSemiBold: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_600SemiBold',
+    },
+    footerBold: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_700Bold',
+    },
+    footerExtraBold: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_800ExtraBold',
+    },
+    footerBlack: {
+      fontSize: 12,
+      lineHeight: 16,
+      letterSpacing: 0,
+      fontFamily: 'Raleway_900Black',
+    },
+  };
+  
+  export default typography;
+  
