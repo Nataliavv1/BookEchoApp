1492cc6c4d6cb13ca026be1a0f07ac37fed1c744
nuriavt1 <134273029+nuriavt1@users.noreply.github.com>
2025-05-19 20:25:24 +0200

    hkjh

--- BookEchoApp/Model/FetchBook.js
+++ BookEchoApp/Model/FetchBook.js
@@ -29,3 +29,4 @@ export async function FetchBook(BOOK_ID = "xYotngEACAAJ") {
     throw error;
   }
 }
+
--- BookEchoApp/Model/SearchISBNBooks.js
+++ BookEchoApp/Model/SearchISBNBooks.js
@@ -1,1 +1,17 @@
 //https://www.googleapis.com/books/v1/volumes?q=isbn: 
+
+const api_url = "https://www.googleapis.com/books/v1/volumes?q=isbn: "
+
+fetch("https://www.googleapis.com/books/v1/volumes?q=isbn:0747532699&key=AIzaSyAdrMfk5xKeXebgngAXQjrKshHuhAAklyM")
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(result) {
+    title = result.items[0].volumeInfo.title;
+    description = result.items[0].volumeInfo.description;
+    console.log(title);
+    console.log(description);
+  }),
+  function(error) {
+    console.log(error);
+  };
