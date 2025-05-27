7f37d7596c7f49a713b1ae3ed0f06952b3379a11
nuriavt1 <134273029+nuriavt1@users.noreply.github.com>
2025-05-27 17:29:08 +0200

    f

--- BookEchoApp/Model/BookTableModel.js
+++ BookEchoApp/Model/BookTableModel.js
@@ -10,38 +10,6 @@ export async function checkBookExists(bookId) {
   return data !== null;
 }
 
-/*
-export async function SaveBook(book) {
-
-  //Autors i categories es una array
-  const autors = Array.isArray(book.autors) ? book.autors.join(', ') : book.autors;
-  const categories = Array.isArray(book.categories) ? book.categories.join(', ') : book.categories;
-
-  const { data, error } = await supabase
-    .from('llibre')
-    .insert([
-      {
-        id: book.id,
-        isbn: book.isbn,
-        descripcio: book.descripcio,
-        autors,
-        categories,
-        imatge: book.imatge,
-        titol: book.titol,
-        puntuaciogoogle: book.puntuaciogoogle,
-        npuntuaciogoogle: book.npuntuaciogoogle,
-        puntuaciomitjana: book.puntuaciomitjana,
-        npuntuaciomitjana: book.npuntuaciomitjana,
-      },
-    ]);
-
-  if (error) {
-    console.error('Error al guardar llibre a la base de dades:', error);
-    return null;
-  }
-
-  return data;
-}*/
 export async function SaveBook(book) {
  console.log('Libro a guardar:', book);
   // Autors i categories es una array
